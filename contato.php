05895d26a30e66fd6fcc2f1d1ee5908eb3b1bfca
Murilo Henrique <murilomatias@alunos.utfpr.edu.br>
2018-10-06 01:11:13 -0300

    Pre made

---
+++ contato.php
@@ -0,0 +1,8 @@
+<?php
+    include_once 'View/_header.php';
+    include_once 'View/_nav.php';
+?>
+<!-- sections -->
+<?php
+    include_once 'View/_footer.php';
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    include_once 'View/_header.php';
+    include_once 'View/_nav.php';
+    include_once 'View/_section.php';
+?>
+<!-- sections -->
+<?php
+    include_once 'View/_footer.php';
+?>
