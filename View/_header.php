1a4d6905d2a3ca563f8e7a8a36e3aaa371730554
Murilo Henrique <murilomatias@alunos.utfpr.edu.br>
2018-10-07 02:11:13 -0300

    Novo estilo definido; Nav desktop e mobile diferentes

--- View/_header.php
+++ View/_header.php
@@ -5,11 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="View/css/estilo.css">
         <script src="View/js/scripts.js"></script>
-        <title>Título</title>
+        <title>Bebê Coelinho</title>
     </head>
     <body>
         <header>
-            <h1>Título</h1>
-            <h2>Artesanato em Crochê</h2>
+            <h1>Bebê Coelinho</h1>
+            <h3>Artesanato em Crochê</h3>
+            <nav id="mobile">
+                <button onclick="location='index.php'">Categorias</button>
+                <button onclick="location='contato.php'">Contato</button>
+            </nav>
         </header>
         <div id="content">
--- View/_nav.php
+++ View/_nav.php
@@ -1,4 +1,8 @@
-<nav>
-    <button onclick="location='index.php'">Categorias</button>
+<nav id="desktop">
+    <button onclick="location='index.php'">Sapatinhos</button>
+    <button onclick="location='index.php'">Casacos</button>
+    <button onclick="location='index.php'">Sandálias</button>
+    <button onclick="location='index.php'">Chapéus e Gorros</button>
+    <button onclick="location='index.php'">Bonecos</button>
     <button onclick="location='contato.php'">Contato</button>
 </nav>
